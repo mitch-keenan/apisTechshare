53733e4ad127e72cdbfd7d464a6ee919bdc02b7a
Mitch Keenan <konamacona@gmail.com>
2018-11-06 09:03:09 -0400

    Initial commit

---
+++ theme.js
@@ -0,0 +1,31 @@
+// example theme.js
+import { future } from "mdx-deck/themes";
+import atomDark from "react-syntax-highlighter/styles/prism/atom-dark";
+import prismJSON from "react-syntax-highlighter/languages/prism/json";
+
+export const theme = {
+  ...future,
+  colors: {
+    ...future.colors,
+    // Dark: #333031
+    // Red: #fc363f
+    // White: white
+    text: "white",
+    background: "#333031",
+    link: "#fc363f"
+  },
+  heading: {
+    ...future.heading,
+    "letter-spacing": "unset"
+    // "font-family": "'Tinos', serif"
+  },
+  paragraph: {
+    "text-align": "left"
+  },
+  prism: {
+    style: atomDark,
+    languages: {
+      json: prismJSON
+    }
+  }
+};
